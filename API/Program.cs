839fad2b98631288c08a1e3399da98e25dd4baa6
Tom Kedem <tompha1@hotmail.com>
2022-11-15 07:03:32 +0200

    End of section 2

--- API/Program.cs
+++ API/Program.cs
@@ -6,7 +6,7 @@ string connString = builder.Configuration.GetConnectionString("DefaultConnection
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(connString);
+  options.UseSqlServer(connString);
 
 });
 // Add services to the container.
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 //     options.UseSqlServer(getConnectionString("Defaultconnection"));
 // });
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,11 +25,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "https://localhost:5001"));
 
 app.UseAuthorization();
 
